13406d7d54581cedaff8eb61e676bc7c226d045f
arminus <arminus@gmail.com>
2021-02-02 11:27:58 +0100

    don't touch the test script(s)

--- export_notebooks.bat
+++ export_notebooks.bat
@@ -1,3 +1,6 @@
-jupyter nbconvert --to script *.ipynb
-dos2unix *.py
-move *.py python-out
+@ECHO OFF
+for /r %%f in (*.ipynb) do (
+    jupyter nbconvert --to script %%f
+    dos2unix "%%~nf.py"
+    move "%%~nf.py" python-out
+)
