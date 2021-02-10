e4a8053ab4fd74d4895ab45c6605c661fd185747
arminus <arminus@gmail.com>
2021-02-10 09:33:25 +0100

    show last update time, download tab

--- html/schongebiete.js
+++ html/schongebiete.js
@@ -56,6 +56,9 @@ fetch("data/statistics.json")
         total += parseInt(value)
         document.querySelector('#'+key).innerHTML = value;
       });
+      document.querySelectorAll('.lastUpdate').forEach(e => {
+          e.innerHTML = json.lastUpdate;
+      });
       document.querySelector('#total').innerHTML = total;
       document.querySelector('#multitypes').innerHTML = json.multiTypes.length;
       document.querySelector('#unclassifieds').innerHTML = json.unclassifieds.length;
